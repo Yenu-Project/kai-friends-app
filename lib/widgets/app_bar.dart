2977935f43afc612d8157b04aa50d7205bbb34d9
Jeesoo Yoon <jeesooyoon@kaist.ac.kr>
2022-01-26 14:18:23 +0900

    feat(widget): add bottom nav bar

---
+++ lib/widgets/app_bar.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class BottomNavBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.search_rounded),
+          label: '친구 찾기',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.send_rounded),
+          label: '받은 신청',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.people_alt_rounded),
+          label: '친구 목록',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.settings),
+          label: '설정',
+        ),
+      ],
+      currentIndex: 0,
+      type: BottomNavigationBarType.fixed,
+      unselectedItemColor: Colors.grey[500],
+      selectedItemColor: Colors.amber[800],
+      showUnselectedLabels: true,
+    );
+  }
+}
