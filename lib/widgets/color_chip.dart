983a94c826d3a0a80598cbc110cecab96d47ef76
Triangleyj <dpwns99@gmail.com>
2022-02-02 17:30:59 +0900

    feat(color chip): add text parameter in color chip

--- lib/widgets/color_chip.dart
+++ lib/widgets/color_chip.dart
@@ -4,7 +4,6 @@ class ColorChip extends StatelessWidget {
   final String text;
 
   const ColorChip({Key? key, this.text = 'chip'}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Chip(
