c5a8b19583fddc574091d2b080c8d67ddb1ae5c1
Triangleyj <dpwns99@gmail.com>
2022-02-06 15:47:44 +0900

    feat(color chip): add onPressed parameter

--- lib/widgets/color_chip.dart
+++ lib/widgets/color_chip.dart
@@ -2,19 +2,34 @@ import 'package:flutter/material.dart';
 
 class ColorChip extends StatelessWidget {
   final String text;
+  final VoidCallback? onPressed;
 
-  const ColorChip({Key? key, this.text = 'chip'}) : super(key: key);
+  const ColorChip({
+    Key? key,
+    this.text = 'chip',
+    this.onPressed,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Chip(
-      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-      label: Text(
-        text,
-        style: const TextStyle(
-          fontSize: 12.0,
 
-        ),
+    Text label = Text(
+      text,
+      style: const TextStyle(
+        fontSize: 12.0,
       ),
     );
+
+    return onPressed != null
+        ? ActionChip(
+            materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            label: label,
+            onPressed: onPressed!,
+          )
+        : Chip(
+            materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            label: label,
+          );
   }
 }
+
