d180abb7052a441c86fbf760a8423a57c971a7fd
kjy2844 <kjy2844@kaist.ac.kr>
2022-02-25 20:36:26 +0900

    refactor(friend list): change MyApp to FriendListScreen

--- lib/screens/friend/friend_list.dart
+++ lib/screens/friend/friend_list.dart
@@ -2,30 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import '../../widgets/color_chip.dart';
 import '../../widgets/top_app_bar.dart';
-import '../../widgets/bottom_nav_bar.dart';
-
-void main() {
-  runApp(MyApp(
-    friends: [
-      Friend(
-          'name1', 'introduction1', ['prefer', 'prefer', 'prefer', 'prefer']),
-      Friend(
-          'name2', 'introduction2', ['prefer', 'prefer', 'prefer', 'prefer']),
-      Friend(
-          'name3', 'introduction3', ['prefer', 'prefer', 'prefer', 'prefer']),
-      Friend(
-          'name4', 'introduction4', ['prefer', 'prefer', 'prefer', 'prefer']),
-      Friend(
-          'name5', 'introduction5', ['prefer', 'prefer', 'prefer', 'prefer']),
-      Friend(
-          'name6', 'introduction6', ['prefer', 'prefer', 'prefer', 'prefer']),
-      Friend(
-          'name7', 'introduction7', ['prefer', 'prefer', 'prefer', 'prefer']),
-      Friend(
-          'name8', 'introduction8', ['prefer', 'prefer', 'prefer', 'prefer']),
-    ],
-  ));
-}
 
 class Friend {
   String name;
@@ -35,19 +11,27 @@ class Friend {
   Friend(this.name, this.introduction, this.preferences);
 }
 
-class MyApp extends StatefulWidget {
-  final List<Friend> friends;
+class FriendListScreen extends StatefulWidget {
+  final List<Friend> friends = List<Friend>.from([
+    Friend('name1', 'introduction1', ['prefer', 'prefer', 'prefer', 'prefer']),
+    Friend('name2', 'introduction2', ['prefer', 'prefer', 'prefer', 'prefer']),
+    Friend('name3', 'introduction3', ['prefer', 'prefer', 'prefer', 'prefer']),
+    Friend('name4', 'introduction4', ['prefer', 'prefer', 'prefer', 'prefer']),
+    Friend('name5', 'introduction5', ['prefer', 'prefer', 'prefer', 'prefer']),
+    Friend('name6', 'introduction6', ['prefer', 'prefer', 'prefer', 'prefer']),
+    Friend('name7', 'introduction7', ['prefer', 'prefer', 'prefer', 'prefer']),
+    Friend('name8', 'introduction8', ['prefer', 'prefer', 'prefer', 'prefer']),
+  ]);
 
-  const MyApp({
+  FriendListScreen({
     Key? key,
-    required this.friends,
   }) : super(key: key);
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  State<FriendListScreen> createState() => _FriendListScreenState();
 }
 
-class _MyAppState extends State<MyApp> {
+class _FriendListScreenState extends State<FriendListScreen> {
   String dropdownValue = '정렬';
   List<String> sortingType = ['정렬', '추가순', '학과순', '학번순'];
   List<Friend> friends = [];
@@ -60,55 +44,52 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: TopAppBar(
-          title: '친구 목록',
-          actions: [
-            DropdownButton<String>(
-              value: dropdownValue,
-              icon: const Icon(Icons.arrow_drop_down),
-              underline: const SizedBox(),
-              items: sortingType
-                  .map((value) => DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      ))
-                  .toList(),
-              onChanged: (String? newValue) {
-                setState(() {
-                  dropdownValue = newValue!;
-                });
-              },
-            ),
-            TextButton.icon(
-                style: TextButton.styleFrom(
-                  primary: Colors.black,
-                ),
-                onPressed: () {},
-                icon: const Icon(Icons.filter_list),
-                label: const Text('필터')),
-          ],
-        ),
-        body: SafeArea(
-          child: ListView.builder(
-            itemCount: widget.friends.length,
-            itemBuilder: (context, index) {
-              return FriendListTile(
-                key: Key(friends[index].name),
-                friend: friends[index],
-                onPressedDeletion: () {
-                  setState(() {
-                    friends.removeAt(index);
-                  });
-                },
-                onPressedMove: () {},
-              );
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: TopAppBar(
+        title: '친구 목록',
+        actions: [
+          DropdownButton<String>(
+            value: dropdownValue,
+            icon: const Icon(Icons.arrow_drop_down),
+            underline: const SizedBox(),
+            items: sortingType
+                .map((value) => DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    ))
+                .toList(),
+            onChanged: (String? newValue) {
+              setState(() {
+                dropdownValue = newValue!;
+              });
             },
           ),
+          TextButton.icon(
+              style: TextButton.styleFrom(
+                primary: Colors.black,
+              ),
+              onPressed: () {},
+              icon: const Icon(Icons.filter_list),
+              label: const Text('필터')),
+        ],
+      ),
+      body: SafeArea(
+        child: ListView.builder(
+          itemCount: widget.friends.length,
+          itemBuilder: (context, index) {
+            return FriendListTile(
+              key: Key(friends[index].name),
+              friend: friends[index],
+              onPressedDeletion: () {
+                setState(() {
+                  friends.removeAt(index);
+                });
+              },
+              onPressedMove: () {},
+            );
+          },
         ),
-        bottomNavigationBar: BottomNavBar(),
       ),
     );
   }
